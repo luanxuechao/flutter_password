ca7e06ddaec4e6653dc24837b4696b3a0b0bc557
luanx <xuechao.luan@autodesk.com>
2020-04-24 18:05:27 +0800

    Add CheckBox

--- lib/pages/password_set.dart
+++ lib/pages/password_set.dart
@@ -13,7 +13,8 @@ class PasswordSetState extends State<PasswordSet> {
   String _name;
 
   String _password;
-
+  bool _favourite = false;
+  bool _requiredPassword = false;
   void _forSubmitted() {
     var _form = _formKey.currentState;
 
@@ -105,14 +106,17 @@ class PasswordSetState extends State<PasswordSet> {
               ),
               new TextFormField(
                 decoration: new InputDecoration(
-                  labelText: 'Password:',
-                   suffix: IconButton(
-                      icon: Icon(Icons.autorenew),
-                      color: Colors.grey,
-                      onPressed: () => {},
-                      tooltip: 'generate',
-                    )
-                ),
+                    labelText: 'Password:',
+                    isDense: true,
+                    // suffixIcon: Icon(Icons.update),
+                    contentPadding: EdgeInsets.only(top: 5),
+                    // contentPadding:EdgeInsets.all(0),
+                    suffix: IconButton(
+                      icon: Icon(Icons.update),
+                      onPressed: () {},
+                      padding: EdgeInsetsDirectional.only(end: 10.0),
+                    ),
+                    suffixStyle: TextStyle(height: 0.1)),
                 obscureText: true,
                 validator: (val) {
                   return val.length < 4 ? "密码长度错误" : null;
@@ -123,15 +127,46 @@ class PasswordSetState extends State<PasswordSet> {
               ),
               new TextFormField(
                 decoration: new InputDecoration(
-                    labelText: 'Notes:',
-                   ),
+                  labelText: 'Notes:',
+                ),
                 validator: (val) {
                   return val.length < 4 ? "密码长度错误" : null;
                 },
+                maxLines: 3,
                 onSaved: (val) {
                   _password = val;
                 },
               ),
+              Row(
+                children: <Widget>[
+                  Checkbox(
+                    value: _favourite,
+                    onChanged: (bool newValue) {
+                      setState(() {
+                        _favourite = !_favourite;
+                      });
+                    },
+                    checkColor: Colors.white,
+                    activeColor: new Color(0xff075E54),
+                  ),
+                  Expanded(child: Text('Favourite')),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Checkbox(
+                    value: _requiredPassword,
+                    onChanged: (bool newValue) {
+                      setState(() {
+                        _requiredPassword = newValue;
+                      });
+                    },
+                    checkColor: Colors.white,
+                    activeColor: new Color(0xff075E54),
+                  ),
+                  Expanded(child: Text('Require Password Repormpt')),
+                ],
+              )
             ],
           ),
         ),
